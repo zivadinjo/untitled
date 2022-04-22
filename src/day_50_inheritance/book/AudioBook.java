ad66dd453f83c5d4e1c02c8a084fec955a074dca
zivadinjo <zivadinjo1985@gmail.com>
2022-04-22 14:46:57 -0500

    changes

---
+++ src/day_50_inheritance/book/AudioBook.java
@@ -0,0 +1,24 @@
+package day_50_inheritance.book;
+
+public class AudioBook extends Book{
+    double duration;
+    String narrator;
+
+    public void listen(){
+        System.out.println("Listening to " + title + " narrated by " + narrator);
+    }
+}
+// AudioBook is a Book
+
+/*
+Create a class AudioBook
+
+    - AudioBook class inherits Book class
+
+    - create additional variables:
+        duration, narrator
+
+    - create method:
+        listen()
+            Example output: prints listening to $title narrated by $narrator
+ */
---
+++ src/day_50_inheritance/book/Book.java
@@ -0,0 +1,16 @@
+package day_50_inheritance.book;
+
+public class Book {
+    String title;
+    Author author;
+    String genre;
+    int chapters;
+    double price;
+    boolean hasAMovie;
+}
+/*
+Create a Book class
+
+    - create variables:
+        title, author, genre, chapter, price, has a movie
+ */
