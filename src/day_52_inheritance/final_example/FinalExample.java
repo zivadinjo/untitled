c56cb1b276e0fcc22791610df68a875936662094
zivadinjo <zivadinjo1985@gmail.com>
2022-04-26 19:17:18 -0500

    changes

---
+++ src/day_52_inheritance/final_example/FinalExample.java
@@ -0,0 +1,15 @@
+package day_52_inheritance.final_example;
+
+public final class FinalExample {
+
+    final int a = 5;
+    final int b;
+
+    public static final String PLANET = "Earth";
+
+    public FinalExample(int b){
+        this.b = b;
+    }
+
+
+}
