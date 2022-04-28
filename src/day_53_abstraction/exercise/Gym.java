981c1c49a6bf1c313db0e8e04eca36957ca33684
zivadinjo <zivadinjo1985@gmail.com>
2022-04-27 21:13:32 -0500

    changes

---
+++ src/day_53_abstraction/exercise/Gym.java
@@ -0,0 +1,14 @@
+package day_53_abstraction.exercise;
+
+public class Gym {
+    public static void main(String[] args) {
+
+        //        Exercise obj1 = new Exercise(""); this class is abstract so you can't create an object
+
+        Running obj2 = new Running();
+        System.out.println(obj2.name);
+        obj2.performExercise();
+        System.out.println(obj2.getCaloriesBurned(20));
+
+    }
+}
