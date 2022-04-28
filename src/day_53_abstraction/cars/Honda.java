bf79d505a8aa3eee7bf923bf9112c22933c96e9e
zivadinjo <zivadinjo1985@gmail.com>
2022-04-27 19:38:58 -0500

    changes

---
+++ src/day_53_abstraction/cars/Honda.java
@@ -0,0 +1,10 @@
+package day_53_abstraction.cars;
+
+public class Honda extends Car{
+
+    @Override
+    public void start(){
+        System.out.println("Press the button");
+    }
+
+}
---
+++ src/day_53_abstraction/cars/Toyota.java
@@ -0,0 +1,11 @@
+package day_53_abstraction.cars;
+
+public class Toyota extends Car{
+
+    @Override
+    public void start(){
+        System.out.println("Insert key");
+        System.out.println("Turn the key");
+    }
+
+}
