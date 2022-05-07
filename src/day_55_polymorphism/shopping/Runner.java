24cc736d704b8e4b04a8b53d7f0ad7164b7f963c
zivadinjo <zivadinjo1985@gmail.com>
2022-05-07 10:38:58 -0500

    day 55 polymorphism

--- src/day_55_polymorphism/shopping/Runner.java
+++ src/day_55_polymorphism/shopping/Runner.java
@@ -12,16 +12,21 @@ public class Runner {
 
         Target target = new Target();
         target.price = 10;
+        System.out.println(target.price);
         target.buyItem();
         target.returnItem();
 //        target.payForShipping(true); Target class does not implement the Shipping interface
 
         Amazon amazon = new Amazon();
         amazon.price = 1000;
+        System.out.println(amazon.price);
         amazon.buyItem();
         amazon.returnItem();
         amazon.payForShipping(true);
         amazon.viewCart();
+        System.out.println(amazon.country);
+
+        System.out.println(Shipping.country); // proper way to access static information it the interface
 
 
     }
--- src/day_55_polymorphism/shopping/Shipping.java
+++ src/day_55_polymorphism/shopping/Shipping.java
@@ -2,6 +2,8 @@ package day_55_polymorphism.shopping;
 
 public interface Shipping {
 
+    String country = "US";
+
     void payForShipping(boolean b);
 
 }
