b8f310749f560a1337665f803e808e86d32bfdd1
zivadinjo <zivadinjo1985@gmail.com>
2022-05-11 12:23:04 -0500

    practice

---
+++ src/practice/mixed/EvenOdd.java
@@ -0,0 +1,28 @@
+package practice.mixed;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class EvenOdd {
+    public static void main(String[] args) {
+
+        String strEven = "";
+        String strOdd = "";
+        int[] nums = {3, 16, 25, 184, 25, 33};
+
+
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] % 2 == 0) {
+
+                strEven += nums[i] + " , ";
+            } else {
+
+                strOdd += nums[i] + " , ";
+            }
+        }
+        System.out.println(strEven);
+        System.out.println(strOdd);
+    }
+}
+
+
