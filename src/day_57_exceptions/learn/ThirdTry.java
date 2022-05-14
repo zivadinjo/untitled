782612ad200f47ddefe2b2a8cb715ec83b1529b2
zivadinjo <zivadinjo1985@gmail.com>
2022-05-14 14:43:28 -0500

    day 57 exception

--- src/day_57_exceptions/learn/ThirdTry.java
+++ src/day_57_exceptions/learn/ThirdTry.java
@@ -11,8 +11,19 @@ public class ThirdTry {
         try {
             System.out.println("Enter a number");
             int num = input.nextInt();
+
+            System.out.println("Enter number two");
+            int num2 = input.nextInt();
+
+            System.out.println(num/num2);
+
         } catch (InputMismatchException e) {
             e.printStackTrace();// show exemption message in the console when wrong input is given(string or char instead of number)
+        } catch (ArithmeticException e){
+            System.out.println(e.getMessage());
+        }finally {     // final block is optional code and is guaranteed to run no matter what
+            System.out.println("FINAL BLOCK");
+            input.close(); // proper way to close scanner
         }
 
         System.out.println("DONE");
