4af78008fe6ed179e2caf93daf56becfbcd5f505
zivadinjo <zivadinjo1985@gmail.com>
2022-05-15 11:48:52 -0500

    day 58 exception

---
+++ src/day_58_exceptions/Person.java
@@ -0,0 +1,33 @@
+package day_58_exceptions;
+
+public class Person {
+
+    private String name;
+    private int age;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) throws Exception {
+
+        if (name == null || name.isEmpty()){
+            throw new Exception("Name can not be null or empty");
+        }
+
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+
+        if (age <= 0 || age >= 120) {
+            throw new  IllegalArgumentException ("Age should be more than 0 or less than 120");
+        }
+
+        this.age = age;
+    }
+}
---
+++ src/day_58_exceptions/PersonObjects.java
@@ -0,0 +1,18 @@
+package day_58_exceptions;
+
+public class PersonObjects {
+    public static void main(String[] args) {
+
+        Person obj = new Person();
+        obj.setAge(50);
+        try {
+            obj.setName("James Bond");
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+
+        System.out.println(obj.getName());
+        System.out.println(obj.getAge());
+
+    }
+}
