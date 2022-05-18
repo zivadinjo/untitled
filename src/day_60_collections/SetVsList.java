9404b9e8409ad439ff010ec94fb5933bcb48b2c8
zivadinjo <zivadinjo1985@gmail.com>
2022-05-18 18:49:43 -0500

    day 60 collection

---
+++ src/day_60_collections/SetVsList.java
@@ -0,0 +1,43 @@
+package day_60_collections;
+
+import java.util.*;
+
+public class SetVsList {
+    public static void main(String[] args) {
+
+        List<Integer> list = new ArrayList<>();
+        list.add(4);
+        list.add(10);
+        list.add(1);
+        list.add(2);
+        list.add(-5);
+        list.add(10);
+        list.add(4);
+        System.out.println(list);
+        System.out.println("first: " + list.get(0));
+        System.out.println("last: "+ list.get(list.size()-1));
+
+        Set<Integer> set = new HashSet<>();
+        set.add(4);
+        set.add(10);
+        set.add(1);
+        set.add(2);
+        set.add(-5);
+        set.add(10);
+        set.add(4);
+        System.out.println(set);
+//        System.out.println("first: " + list.get(0));
+//        System.out.println("last: "+ list.get(list.size()-1));
+        //set does not have indexes
+
+        for (int each : set){
+            System.out.println(each);
+        }
+
+        //sort my HashSet
+
+        System.out.println(new TreeSet<>(set));
+
+
+    }
+}
