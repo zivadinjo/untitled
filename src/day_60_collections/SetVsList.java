29909da6297b1afd54c2a91c43a132839b56af76
zivadinjo <zivadinjo1985@gmail.com>
2022-05-18 19:41:28 -0500

    day 60 collection

--- src/day_60_collections/SetVsList.java
+++ src/day_60_collections/SetVsList.java
@@ -30,6 +30,7 @@ public class SetVsList {
 //        System.out.println("last: "+ list.get(list.size()-1));
         //set does not have indexes
 
+        // because set does not have indexes, we can use a loop to go through the elements
         for (int each : set){
             System.out.println(each);
         }
@@ -37,6 +38,27 @@ public class SetVsList {
         //sort my HashSet
 
         System.out.println(new TreeSet<>(set));
+
+        //convert List to Set
+
+        System.out.println(new LinkedHashSet<>(list));
+
+        System.out.println(new HashSet<>(Arrays.asList(1, 5, 2, 1, 4, 2, 5, 1))); // asList method return List type, which can have the reference of the Collection interface, so we are able to make an object of HashSet with these initial values
+
+        String s = "aaaabbbbcccccddddeee";
+        System.out.println(new HashSet<>(Arrays.asList(s.split("")))
+                .toString()
+                .replace(", ", "")
+                .replace("[", "")
+                .replace("]", ""));
+
+        /* above we convert the String s to an array
+            that array is the argument of the Arrays.asList method()
+               that array is converted to as List type which is allowed in the HashSet constructor
+               because List is a Collection and Collection is the parameter type
+               convert the Hashset - which removed the duplicates, to a String and did String manipulation
+
+         */
 
 
     }
