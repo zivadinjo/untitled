e55e19dd461b01c43228d30362bd37815dd868d9
zivadinjo <zivadinjo1985@gmail.com>
2022-05-18 20:53:03 -0500

    day 60 collection

---
+++ src/day_60_collections/QueueObjects.java
@@ -0,0 +1,23 @@
+package day_60_collections;
+
+import java.util.PriorityQueue;
+
+public class QueueObjects {
+    public static void main(String[] args) {
+
+        PriorityQueue<Integer> queue = new PriorityQueue<>();
+        queue.add(2);
+        queue.add(10);
+        queue.add(8);
+        queue.add(1);
+        queue.add(1);
+        System.out.println(queue);
+
+        queue.offer(30); // same as add method
+        System.out.println(queue);
+
+//        System.out.println(new PriorityQueue<>().remove()); --> causes exception because there is no elements
+        System.out.println(new PriorityQueue<>().poll()); // causes exception /null/ because there is no elements
+
+    }
+}
